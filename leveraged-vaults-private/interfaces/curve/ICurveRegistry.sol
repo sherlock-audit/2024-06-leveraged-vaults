cbc1e5f3b4e66a1f6da3f216052c61e94c317060
Fran Rimoldi <francisco.rimoldi@gmail.com>
2024-09-05 08:40:49 -0300

    fix: remove private directory

--- leveraged-vaults-private/interfaces/curve/ICurveRegistry.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.7.6;
-
-interface ICurveRegistry {
-    function find_pool_for_coins(address _from, address _to, uint256 i)
-        external
-        view
-        returns (address);
-}
